4eab7eda2de53da400fac055578adc9e22b7d0b1
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2011-06-12 20:30:05 +0530

    wired up the setup code

--- lib/dnotify.rb
+++ lib/dnotify.rb
@@ -1,21 +1,25 @@
 module Dnotify
   class Notifier
+    require 'fileutils'
 
-    ConfigPath = "~/.dnotifyrc"
+    ConfigPath = File.expand_path "~/.dnotifyrc"
 
 
     def self.check
-      File.exists(ConfigPath)
+      File.exists?(ConfigPath)
     end
 
     def self.setup
       return if check || !confirm_setup
+      FileUtils.cp(File.join(File.dirname(__FILE__), '../resources/dnotifyrc.sample.yaml'), ConfigPath)
+      puts "copied default template to #{ConfigPath}"
     end
 
     def self.confirm_setup
-      puts "You dont' seem to have the dnotify setup, do you want an empty config file(ConfigPath) to be created?(yes/y/no/n)"
-      answer = gets
-      return answer =~ /(y|yes)/i
+      puts "You dont' seem to have the dnotify setup, do you want an empty config file(#{ ConfigPath }) to be created?(yes/y/no/n)"
+      answer = !!(gets =~ /(y|yes)/i)
+      puts "Exiting dnotify" if !answer
+      answer
     end
   end
 end
