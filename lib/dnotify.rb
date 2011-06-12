ff861df5a6e28f590774dc99c1423bad31d373f0
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2011-06-12 23:42:30 +0530

    fixed crontab

--- lib/dnotify.rb
+++ lib/dnotify.rb
@@ -22,7 +22,7 @@ module Dnotify
       puts 'setting up cron'
       tmp_cron_file = Tempfile.new('dnotify_tmp_cron').path
       File.open(tmp_cron_file, File::WRONLY | File::APPEND) do |file|
-        file << existing_crontab << "* * * * * /bin/bash -l -c 'dnotify' #dnotify\n"
+        file << existing_crontab << "* * * * * /bin/bash -l -c 'dnotify' >> /tmp/dnotify.log\n"
       end
       puts `crontab #{tmp_cron_file}`
     end
